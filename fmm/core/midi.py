effe364a6137d46eb1e745614ab717103f634297
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-05-29 18:14:55 +0100

    change pattern to contain a list of MIDI messages instead of Note objects

--- fmm/core/midi.py
+++ fmm/core/midi.py
@@ -6,12 +6,12 @@ import fmm.core.status as status
 import fmm.core.params as params
 
 def play_midi(port, midi_file):
-    for message in midi_file:
+    for message in midi_file.play():
         if not status.is_playing:
             break
-        time.sleep(message.time)
         if not message.is_meta:
             port.send(message)
+            print(message)
 
 def _next_fractal(note_numbers, figures, depth, branching_factor, octave_spread):
     pattern = random_pattern(note_numbers, figures, 4)
@@ -20,22 +20,11 @@ def _next_fractal(note_numbers, figures, depth, branching_factor, octave_spread)
 
     return fractal
 
-def infinite_play(midi_port):
-    note_numbers = theory.notes_in_key(params.key)
-    fractal = _next_fractal(note_numbers, params.figures, params.depth, params.branching_factor, params.octave_spread)
-
+def infinite_play(midi_port, callback):
     while status.is_playing:
-        # change pattern with a probability
-        if random.random() < params.change_prob or status.params_changed:
-            note_numbers = theory.notes_in_key(params.key)
-            fractal = _next_fractal(note_numbers, params.figures, params.depth, params.branching_factor, params.octave_spread)
-
-            status.params_changed = False
-
-        mid = generate_midi(fractal, params.branching_factor, params.depth, params.bpm)
+        # status.params_changed = False
+        fractal = callback()
+        mid = generate_midi(fractal, params.depth, params.bpm)
         play_midi(midi_port, mid)
 
     midi_port.reset()
-
-def get_midi_input(message):
-    print(message)
--- fmm/core/pattern.py
+++ fmm/core/pattern.py
@@ -1,24 +1,41 @@
-from fmm.core.note import Note
-
 class Pattern:
-    def __init__(self, notes):
-        self.notes = notes
-        self.order = 0
+    def __init__(self, messages):
+        self.messages = list(map(lambda msg: msg.copy(), messages))
+        self._order = 0
+
+    @property
+    def order(self):
+        return self._order
+
+    @order.setter
+    def order(self, value):
+        for i in range(len(self.messages)):
+            if not self.messages[i].is_meta:
+                self.messages[i] = self.messages[i].copy(channel=value)
+
+        self._order = value
 
     def shift_octave(self, offset):
-        self.notes = list(map(lambda note: Note(note.number + offset * 12, note.duration), self.notes))
+        for i in range(len(self.messages)):
+            self.messages[i].note += offset * 12
+
+    def set_message_times(self, branching_factor):
+        # Skip first message to anchor time at the start
+        for i in range(1, len(self.messages)):
+            time = self.messages[i].time / pow(branching_factor, self.order)
+            self.messages[i] = self.messages[i].copy(time=time)
 
     def __repr__(self):
-        return '<' + str(self.notes) + '|' + str(self.order) + '>'
+        return '<' + str(self.messages) + '|' + str(self.order) + '>'
 
     def __str__(self):
-        return '<' + str(self.notes) + '|' + str(self.order) + '>'
+        return '<' + str(self.messages) + '|' + str(self.order) + '>'
 
     def __len__(self):
-        return len(self.notes)
+        return len(self.messages)
 
     def __getitem__(self, i):
-        return self.notes[i]
+        return self.messages[i]
 
     def __setitem__(self, i, note):
-        self.notes[i] = note
+        self.messages[i] = note
